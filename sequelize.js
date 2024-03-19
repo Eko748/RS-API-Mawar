9ec906fcf648c47f3f4333ca0d6731f769203c38
Eko748 <permanaeko74@gmail.com>
2024-03-19 21:01:55 +0700

    Production

--- sequelize.js
+++ sequelize.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('erp_rs', 'root', '', {
+const sequelize = new Sequelize('u778534470_rs_mawar', 'u778534470_rs_mawar', 'RSMawar123', {
   host: 'localhost',
   dialect: 'mysql',
 });
